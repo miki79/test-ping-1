38ab611f1211b22f8b0c5d199e4cbcb35b61e171
Michele Giardi <michele.giardi@slalom.com>
2019-03-27 11:21:33 +0000

    Change endpoint

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const { getResults } = require('./lib.js');
 
 getResults().then(data => {
   console.info(data);
-  var options = { method: 'POST', url: 'https://enudlh7uqfu5.x.pipedream.net/', body: data, json: true };
+  var options = { method: 'POST', url: 'https://enbuijgo87h2r.x.pipedream.net/', body: data, json: true };
 
   request(options, (error, response, body) => {
     if (error) throw new Error(error);
